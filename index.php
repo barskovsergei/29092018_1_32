bb1a2f7c2e4b5775c8bbcce7d6bb74ae8faaeae4
barskovsergei <ser.barsckov@yandex.ru>
2019-02-03 12:12:10 +0300

    Добавлены папки css images js

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-    echo "Привет, я в GIT";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Это наша супер страница!</h1>
+
+    <script src='/js/main.js'></script>
+</body>
+</html>
